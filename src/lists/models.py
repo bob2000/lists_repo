8b3a9c67e5aeba8dfff93c4a0fdbf07588bbfb1c
bob2000 <ligoo3@gmail.com>
2015-06-17 00:32:36 +0200

    Dostosowanie modelu, aby uzytkownicy mogli miec swojej wlasne listy

--- src/lists/models.py
+++ src/lists/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 from django.template.defaultfilters import default
 
+class List(models.Model):
+    pass
+
 class Item(models.Model):
     text = models.TextField(default='')
+    list = models.ForeignKey(List, default=None)
