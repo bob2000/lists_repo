ef85f0fffa27d6b7e3ebe78745b6954b3e129bac
bob2000 <ligoo3@gmail.com>
2015-10-19 20:56:06 +0200

    first steps of FT for layout + styling

--- src/functional_tests/tests.py
+++ src/functional_tests/tests.py
@@ -23,7 +23,7 @@ class NewVisitorTest(LiveServerTestCase):
         rows = table.find_elements_by_tag_name('tr')
         self.assertIn(row_text, [row.text for row in rows])
         
-    def test_can_start_a_list_and_retrive_it_later(self):
+    def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
         self.browser.get(self.live_server_url)
@@ -95,3 +95,26 @@ class NewVisitorTest(LiveServerTestCase):
 
 # if __name__ == '__main__':
 #     unittest.main(warnings='ignore')
+   
+    def test_layout_and_styling(self):
+        # Edith goes to the home page
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # She notices the input box is nicely centered
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=5
+        )
+        
+        # She starts a new list and sees the input is nicely
+        # centered there too
+        inputbox.send_keys('testing\n')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=5
+        )
