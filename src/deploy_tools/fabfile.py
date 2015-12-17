44dc9402cdda767b805faf041c3de7c0011e5f33
bob2000 <ligoo3@gmail.com>
2015-12-17 01:53:41 +0100

    fabfile poprawki

--- src/deploy_tools/fabfile.py
+++ src/deploy_tools/fabfile.py
@@ -6,7 +6,7 @@ REPO_URL = 'https://github.com/bob2000/lists_repo.git'
 
 def deploy():
     site_folder = '/home/%s/sites/%s' % (env.user, env.host)
-    source_folder = site_folder + '/source/src'
+    source_folder = site_folder + '/source'
     _create_directory_structure_if_necessary(site_folder)
     _get_latest_source(source_folder)
     _update_settings(source_folder, env.host)
@@ -27,33 +27,33 @@ def _get_latest_source(source_folder):
     run('cd %s && git reset --hard %s' % (source_folder, current_commit))
     
 def _update_settings(source_folder, site_name):
-    settings_path = source_folder + '/Python_TDD/settings.py'
+    settings_path = source_folder + '/src/Python_TDD/settings.py'
     sed(settings_path, "DEBUG = True", "DEBUG = False")
     sed(settings_path,
         'ALLOWED_HOSTS =.+$',
         'ALLOWED_HOSTS = ["%s"]' % (site_name,)
     )
-    secret_key_file = source_folder + '/Python_TDD/secret_key.py'
-    if not exists(secret_key_file):  #3
+    secret_key_file = source_folder + '/src/Python_TDD/secret_key.py'
+    if not exists(secret_key_file):
         chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
         key = ''.join(random.SystemRandom().choice(chars) for _ in range(50))
         append(secret_key_file, "SECRET_KEY = '%s'" % (key,))
     append(settings_path, '\nfrom .secret_key import SECRET_KEY')
     
 def _update_virtualenv(source_folder):
-    virtualenv_folder = source_folder + '/../virtualenv'
+    virtualenv_folder = source_folder + '/src/../../virtualenv'
     if not exists(virtualenv_folder + '/bin/pip'):
         run('virtualenv --python=python3 %s' % (virtualenv_folder,))
     run('%s/bin/pip install -r %s/requirements.txt' % (
-            virtualenv_folder, source_folder
+            virtualenv_folder, source_folder + '/src'
     ))
     
 def _update_static_files(source_folder):
-    run('cd %s && ../virtualenv/bin/python3 manage.py collectstatic --noinput' % (
-        source_folder,
+    run('cd %s && ../../virtualenv/bin/python3 manage.py collectstatic --noinput' % (
+        source_folder + '/src',
     ))
     
 def _update_database(source_folder):
-    run('cd %s && ../virtualenv/bin/python3 manage.py migrate --noinput' % (
-        source_folder,
+    run('cd %s && ../../virtualenv/bin/python3 manage.py migrate --noinput' % (
+        source_folder + '/src',
     ))
